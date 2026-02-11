c2b7f624e3dfc9f7dcbf99fd817706c0fd88c8a7
Hunter Clezie <clezhunt@gmail.com>
2026-02-11 08:44:23 -0700

    started the accumulate message function

--- AccumulateMessageFunction/Program.cs
+++ AccumulateMessageFunction/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "";
+            string message = userInput;
+            string accumulatedMessage = AccumulateMessage(message);
+
+            do
+            { 
+                Console.WriteLine(accumulatedMessage);
+                Console.ReadLine();
+
+            } while (userInput != "Q" && userInput != "q") ;
+
+            //pause
+            Console.Read();
+        }
+
+        static string AccumulateMessage(string message)
+        {
+            Console.WriteLine("Enter a message to accumulate (or 'Q' to quit):");
+            string userInput = "";
+            userInput = Console.ReadLine();
+            message = userInput;
+            return $"Accumulated: {message}";
         }
     }
 }
